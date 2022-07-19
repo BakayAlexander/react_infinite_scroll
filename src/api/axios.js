5b79810bec029752065764d3dfb7367c0303df5f
BakayAlexander <bakay.dvr@gmail.com>
2022-07-19 13:44:45 +0300

    clean project

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const api = axios.create({ baseURL: 'http://jsonplaceholder.typicode.com' });
+export const api = axios.create({ baseURL: 'https://jsonplaceholder.typicode.com' });
 
 export const getPostsPage = async (pageParam = 1, options = {}) => {
   const response = await api.get(`/posts?_page=${pageParam}`, options);
